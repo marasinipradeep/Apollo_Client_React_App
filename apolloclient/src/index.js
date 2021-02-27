4a8b45ebe8c47cbb31bc6a56ed7ba68653b9c354
marasinipradeep <marasinipradeep@gmail.com>
2021-02-27 17:11:17 +1030

    Sucessfully retrieved data using gql query

--- apolloclient/src/index.js
+++ apolloclient/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
