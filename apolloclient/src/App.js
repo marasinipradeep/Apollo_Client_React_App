c654611ce87c9470993201f2cfb54d1a17129dec
marasinipradeep <marasinipradeep@gmail.com>
2021-02-28 09:41:48 +1030

    set the notifyOnNetworkStatusChange option to true so our query component re-renders while a refetch is in flight

--- apolloclient/src/App.js
+++ apolloclient/src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { gql } from '@apollo/client';
 
@@ -9,28 +10,37 @@ import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  //uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: "https://71z1g.sse.codesandbox.io/",
   cache: new InMemoryCache()
 });
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 function App() {
+
+  const [selectedDog, setSelectedDog] = useState(null);
+  function onDogSelected({ target }) {
+    setSelectedDog(target.value);
+  }
+
   return (
     <ApolloProvider client={client}>
-      <Dogs/>
-      <DogPhoto/>
-    <ExchangeRate/>
+      <h2>Building Query components 🚀</h2>
+     {selectedDog && <DogPhoto breed={selectedDog} />} 
+      <Dogs onDogSelected={onDogSelected} />
+
+      {/* <ExchangeRate/> */}
 
     </ApolloProvider>
   );
--- apolloclient/src/Components/DogPhoto.js
+++ apolloclient/src/Components/DogPhoto.js
@@ -1,4 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
+//The useQuery hook's result object provides fine-grained information about the status of the query via the networkStatus property. To take advantage of this information, we set the notifyOnNetworkStatusChange option to true so our query component re-renders while a refetch is in flight:
+import { NetworkStatus } from '@apollo/client';
+
 
 const GET_DOG_PHOTO = gql`
   query Dog($breed: String!) {
@@ -10,18 +13,21 @@ const GET_DOG_PHOTO = gql`
 `;
 
 function DogPhoto({ breed }) {
-  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+  const { loading, error, data,refetch,networkStatus } = useQuery(GET_DOG_PHOTO, {
     variables: { breed },
-    pollInterval:500 // you will fetch the current breed's image from the server evry 0.5 seconds.If you set pollInterval to 0, the query will not poll.
+    notifyOnNetworkStatusChange:true
+   // pollInterval:500 // you will fetch the current breed's image from the server evry 0.5 seconds.If you set pollInterval to 0, the query will not poll.
   });
 
+  if(networkStatus === NetworkStatus.refetch) return 'Refetching!';
   if (loading) return null;
   if (error) return `Error! ${error}`;
 
   return (
       <>
-    <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
-    <button onClick={()=>refetch()}>Refetch!</button>
+    <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} alt="dog type"/>
+    {/* Refetching is an excellent way to guarantee fresh data, but it introduces some complexity with loading state.  */}
+    <button onClick={()=>refetch()}>Refetch!</button> 
     </>
   
   );
